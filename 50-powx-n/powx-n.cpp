51a7de2ef38ff6a9d9be2f5a6e5197b630a1ec5e
affanadil <82828943+affanadil@users.noreply.github.com>
2025-10-08 13:48:22 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.44%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
-    }
+        long long temp = n;
+        if (temp < 0)
+        {
+            x = 1/x;
+            temp = (-1) * (temp);
+        }
+        if (temp ==0)
+            return 1;
+        double temp2 = myPow(x,temp/2);
+        if (temp%2 ==0)
+            return temp2*temp2;
+        else 
+            return x*temp2*temp2;
+};
 };
