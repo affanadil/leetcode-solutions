f5ae31763fa0deb831803bdb202e359db5214a68
affanadil <82828943+affanadil@users.noreply.github.com>
2025-11-06 15:42:40 +0500

    Time: 0 ms (100.00%) | Memory: 23.7 MB (98.02%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,17 +3,11 @@ class Solution
 public:
     void moveZeroes(vector<int> &nums)
     {
-        int j = 0;
+        int t = 0;
         for (int i = 0; i < nums.size(); i++)
-        {
             if (nums[i] != 0)
-            {
-                nums[j++] = nums[i];
-            }
-        }
-        while (j < nums.size())
-        {
-            nums[j++] = 0;
-        }
+                nums[t++] = nums[i];        
+        while (t < nums.size())
+            nums[t++] = 0;
     }
 };
